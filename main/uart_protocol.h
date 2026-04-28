278a11cdc0221093700120f0fcdecf03fc9df8a7
Markus Fink <164403707+Thinke314@users.noreply.github.com>
2026-04-28 20:52:21 +0200

    Expand UART protocol status interface

--- main/uart_protocol.h
+++ main/uart_protocol.h
@@ -23,6 +23,8 @@ typedef struct {
     thermowork_process_values_t process_values;
     thermowork_control_config_t config;
     bool command_enable_outputs;
+    bool command_emergency_stop;
+    bool command_reset_fault;
 } thermowork_uart_message_t;
 
 void thermowork_uart_protocol_init(void);
@@ -31,7 +33,10 @@ int thermowork_uart_format_status(
     char *buffer,
     size_t buffer_size,
     const thermowork_control_output_t *output,
-    const char *fault_string
+    const thermowork_control_config_t *config,
+    const thermowork_process_values_t *values,
+    const char *fault_string,
+    bool gpio_level
 );
 
 #ifdef __cplusplus
